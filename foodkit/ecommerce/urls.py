615fb3ab088454e3832319517db64b672f907010
Alvaro <alvaroulloaa@gmail.com>
2020-06-10 18:18:07 -0300

    Second versions

--- foodkit/ecommerce/urls.py
+++ foodkit/ecommerce/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("", views.InicioView.as_view(), name="index"),
     path("producto/<slug>/", views.ItemDetailView.as_view(), name="producto"),
     path("checkout/", views.checkout, name="checkout"),
+    path("resumen-compra/", views.OrderSummaryView.as_view(), name="resumen-compra"),
     path("add-to-cart/<slug>/", views.add_to_cart, name="add-to-cart"),
-    path("remove-from-cart/<slug>/", views.remove_from_cart, name="remove-from-cart")
+    path("remove-from-cart/<slug>/", views.remove_from_cart, name="remove-from-cart"),
+    path("remove-item-from-cart/<slug>/", views.remove_single_item_from_cart, name="remove-single-item-from-cart")
 ]
--- foodkit/ecommerce/views.py
+++ foodkit/ecommerce/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import redirect
 from django.utils import timezone
 
@@ -9,7 +12,20 @@ from .models import Item, OrderItem, Order
 # Create your views here.
 class InicioView(ListView):
     model = Item
+    paginate_by = 8
     template_name = "ecommerce/inicio.html"
+
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order
+            }
+            return render(self.request, "ecommerce/resumen_compra.html", context)
+        except ObjectDoesNotExist:
+            messags.error(self.request, "Todavía no tienes una orden activa.")
+            return redirect("/")
 
 class ItemDetailView(DetailView):
     model = Item
@@ -24,6 +40,7 @@ def productos(request):
 def checkout(request):
     return render(request, "ecommerce/checkout.html")
 
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(item=item, user=request.user, ordered=False)
@@ -34,19 +51,20 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
-            messages.info(request, "Producto añiadido al carrito de compra.")
-            return redirect("ecommerce:producto", slug=slug)
+            messages.info(request, "Cantidad del producto actualizada.")
+            return redirect("ecommerce:resumen-compra")
         else:
             order.items.add(order_item)
-            messages.info(request, "Producto añiadido al carrito de compra.")
-            return redirect("ecommerce:producto", slug=slug)
+            messages.info(request, "Cantidad del producto actualizada.")
+            return redirect("ecommerce:resumen-compra")
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "Producto añiadido al carrito de compra.")
-        return redirect("ecommerce:producto", slug=slug)
+        return redirect("ecommerce:resumen-compra")
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
@@ -57,7 +75,30 @@ def remove_from_cart(request, slug):
             order_item = OrderItem.objects.filter(item=item, user=request.user, ordered=False)[0]
             order.items.remove(order_item)
             messages.info(request, "Producto removido del carrito de compra.")
+            return redirect("ecommerce:resumen-compra")
+        else:
+            messages.info(request, "Este producto no estaba en el carrito de compra.")
             return redirect("ecommerce:producto", slug=slug)
+    else:
+        messages.info(request, "Usted no posee una orden activa.")
+        return redirect("ecommerce:producto", slug=slug)
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(item=item, user=request.user, ordered=False)[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "Cantidad del producto actualizada.")
+            return redirect("ecommerce:resumen-compra")
         else:
             messages.info(request, "Este producto no estaba en el carrito de compra.")
             return redirect("ecommerce:producto", slug=slug)
